a45d828a7720cd0ebeaadda8b7c16783c2513549
oauguste <augusteosnac@gmail.com>
2023-02-15 07:13:42 -0500

    Finished Project
    Finito

--- pokemon-downloader.js
+++ pokemon-downloader.js
@@ -1,13 +1,13 @@
 import { promptUser } from "./promps.js";
 import inquirer from "inquirer";
-import path from "path"
-import fs from "fs/promises"
+import path from "path";
+import fs from "fs/promises";
 import { createFolder } from "./saving.js";
 import {
   savePokemonSprites,
   savePokemonStats,
 } from "./saving.js";
 
-console.log("======= POKEMON DOWNLOADER =======")
+console.log("======= POKEMON DOWNLOADER =======");
 
 promptUser();
